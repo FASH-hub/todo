a3515e1feacc26677567fee43a70b167e3037301
Léon FASHINGABO <53854@etu.he2b.be>
2022-02-15 23:55:20 +0100

    [updates] todo

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -10,7 +10,7 @@ export default function Todo({ todos, isCompleted, removeTask }) {
     return todos.map((todo, index) => (
 
         <div className={todo.isCompleted ? 'complete task-div' : 'task-div'} key={index}>
-            <li className="flex ml-4 p-4 mt-10">{todo.text}
+            <li className="flex ml-4 p-4 mt-2">{todo.text}
                 <TiTick className="ml-2" onClick={() => { isCompleted(todo.id) }}></TiTick>
                 <RiCloseCircleLine className="ml-2" onClick={() => { removeTask(todo.id) }}></RiCloseCircleLine>
             </li>
