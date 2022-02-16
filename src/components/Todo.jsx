1af9cea019b26361286258e3dd766e0c03f2c96d
Léon FASHINGABO <53854@etu.he2b.be>
2022-02-16 12:27:26 +0100

    [updates] images

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import { RiCloseCircleLine } from 'react-icons/ri'
-import { TiTick } from 'react-icons/ti'
+
 
 
 export default function Todo({ todos, isCompleted, removeTask }) {
@@ -8,12 +7,14 @@ export default function Todo({ todos, isCompleted, removeTask }) {
 
 
     return todos.map((todo, index) => (
+        <div className="border-solid border-2 ml-9 mt-4 max-w-sm">
 
-        <div className={todo.isCompleted ? 'complete task-div' : 'task-div'} key={index}>
-            <li className="flex ml-4 p-4 mt-2">{todo.text}
-                <TiTick className="ml-2" onClick={() => { isCompleted(todo.id) }}></TiTick>
-                <RiCloseCircleLine className="ml-2" onClick={() => { removeTask(todo.id) }}></RiCloseCircleLine>
-            </li>
+            <div className={todo.isCompleted ? "line-through" : ""} key={index}>
+                <li className="flex ml-4 p-4 mt-2 ">{todo.text}
+                    <button className="ml-2 bg-red-900 text-white w-5 h-5" onClick={() => { removeTask(todo.id) }}>X</button>
+                    <input className="ml-2 w-7 h-5" onClick={() => { isCompleted(todo.id) }} type="checkbox" defaultChecked={`${todo.isCompleted ? "checked" : ""}`} />
+                </li>
+            </div>
         </div>
     ));
 }
